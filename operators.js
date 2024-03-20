f21ac39a4363eb2865cd8ace59f2a16113dc0833
AkramQureshi <161283057+AkramQureshi@users.noreply.github.com>
2024-03-20 22:57:42 +0500

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,38 @@
+"use strict";
+//comments
+/*let num1=3;
+let num2=5;
+ans = num1+num2;
+console.log(ans);*/
+let num1 = 15;
+let num2 = 10;
+let num3 = 12;
+let answer;
+console.log(num1 + num2 + num3);
+answer = (num1 + num2);
+console.log(answer);
+//Addition
+/*answer=(num1+num2);
+console.log(answer);*/
+/*Subtraction
+
+answer=(num1-num2);
+console.log(answer);*/
+//Division
+/*answer=(num1/num2);
+console.log(answer);*/
+//Multiplication
+/*answer=(num1*num2);
+console.log(answer);*/
+//increament
+/*answer++;
+answer++;
+num3+=8;
+console.log(answer);
+console.log(num3);*/
+//Decreament
+/*answer--;
+answer--;
+num3-=8;
+console.log(answer);
+console.log(num3);*/
---
+++ operators.ts
@@ -0,0 +1,50 @@
+//comments
+/*let num1=3;
+let num2=5;
+ans = num1+num2;
+console.log(ans);*/
+
+let num1: number=15
+let num2: number=10
+let num3: number=12
+let answer: number;
+console.log (num1+num2+num3)
+answer=(num1+num2);
+console.log(answer);
+
+//Addition
+
+/*answer=(num1+num2);
+console.log(answer);*/
+
+/*Subtraction
+
+answer=(num1-num2);
+console.log(answer);*/
+
+//Division
+
+/*answer=(num1/num2);
+console.log(answer);*/
+
+//Multiplication
+
+/*answer=(num1*num2);
+console.log(answer);*/
+
+//increament
+/*answer++;
+answer++;
+num3+=8;
+console.log(answer);
+console.log(num3);*/
+
+
+//Decreament
+/*answer--;
+answer--;
+num3-=8;
+console.log(answer);
+console.log(num3);*/
+
+
